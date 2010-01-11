bdfc926a4833dd4b281533df3c5180216c331fee
DreLnBrown <DreLnBrown@e933ee44-1dc6-11de-9e56-bf19dc6c588e>
2010-01-11 19:02:11 +0000

    - Added Loki lib - Started working on linux platform

---
+++ trunk/GameEngine/Platform/Linux/Platform_Linux.cpp
@@ -0,0 +1,28 @@
+#include "Platform_Linux.hpp"
+#include "SDL/Sdl_util.hpp"
+
+using namespace Loki;
+using namespace Sdl_Util;
+using namespace Spiral;
+
+const Uint32 kSdl_InitFlags = SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_JOYSTICK;
+
+int main( int argc, char *argv[] )
+{
+     try
+        {
+            if( SDL_Succeded( SDL_Init( kSdl_InitFlags ) ) )
+            {
+                ScopeGaurd quitGaurd = MakeGaurd( SDL_Quit );
+            }
+            else
+            {
+                THROW_GENERAL_EXCEPTION( std::string( "Error - Cannot initialize SDL! :" ) + SDL_GetError() );
+            }
+
+        }
+        catch( std::exception& e )
+        {
+        }
+
+}
