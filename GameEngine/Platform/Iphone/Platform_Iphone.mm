4db8a31de3f021940d01437934f3e8205e05ea23
DreLnBrown <DreLnBrown@e933ee44-1dc6-11de-9e56-bf19dc6c588e>
2010-04-29 12:39:50 +0000

    added xcode project

--- GameEngine/Platform/Iphone/Platform_Iphone.mm
+++ GameEngine/Platform/Iphone/Platform_Iphone.mm
@@ -1,9 +1,24 @@
 #import <UIKit/UIKit.h>
+#import <exception>
+#import <iostream>
 
 int main(int argc, char *argv[]) {
     
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, @"Platform_IphoneAppDelegate");
+	int retVal;
+	try 
+	{
+		NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+		retVal = UIApplicationMain(argc, argv, nil, @"Platform_IphoneAppDelegate");
+	}
+	catch ( std::exception e ) 
+	{
+		std::cout << "[Exception Thrown]: " << e.what() << std::endl;
+	}
+	catch (...) 
+	{
+		std::cout << "[Exception Thrown]: unknown exception!" << std::endl;
+	}
+    
     [pool release];
     return retVal;
 }
